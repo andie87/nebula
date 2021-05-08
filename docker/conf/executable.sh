bbfe88a9b3003850e1c9a3f9fd0381c92c764c8c
Andhyruk <andhyruk@gmail.com>
2021-05-09 00:31:58 +0700

    change port

--- docker/conf/executable.sh
+++ docker/conf/executable.sh
@@ -3,7 +3,7 @@
 # Start the first process
 set -m
 
-gunicorn -t 300 --worker-class=gevent --worker-connections=1024 --reload --workers=17 --max-requests 262144 --max-requests-jitter 16384 --worker-tmp-dir /tmp/ --backlog 4096 --chdir /www/project project.wsgi:application --bind 0.0.0.0:8080 &
+gunicorn -t 300 --worker-class=gevent --worker-connections=1024 --reload --workers=17 --max-requests 262144 --max-requests-jitter 16384 --worker-tmp-dir /tmp/ --backlog 4096 --chdir /www/project project.wsgi:application --bind 0.0.0.0:8000 &
 printenv | sed 's/^\(.*\)$/export \1/g' > /root/project_env.sh
 #cron
 
