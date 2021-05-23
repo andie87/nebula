3580ed926549a9a60c2c9e8576a5a2d868e83a65
andhyruk <andhyruk@gmail.com>
2021-05-23 20:57:04 +0700

    hide notification

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notifications',
+    # 'notifications',
     'django_extensions',
     'polymorphic'
 
